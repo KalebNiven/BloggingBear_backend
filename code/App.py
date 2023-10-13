daa9f6cfbc626363b778941208cdd38c79301b63
Usama Gull <usamagull@axiomworld.net>
2023-10-13 16:35:23 +0500

    [MOD] cors origin

--- code/App.py
+++ code/App.py
@@ -12,7 +12,8 @@ from cryptography.fernet import Fernet
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
-CORS(app,resources={r"/*": {"origins": "https://bloggingbear-frontend-39f0be1ffd81.herokuapp.com/"}})
+CORS(app)
+# CORS(app,resources={r"/*": {"origins": "https://bloggingbear-frontend-39f0be1ffd81.herokuapp.com/"}})
 
 # Attempt to load the secret key from an environment variable
 # If it doesn't exist, create a new one and save it in the environment variable
