7c11217863654174212acbccc208a3a82f14ce0b
Usama Gull <usamagull@axiomworld.net>
2023-10-23 22:26:07 +0500

    [Add] more clear try and expect

--- App.py
+++ App.py
@@ -22,6 +22,8 @@ CORS(app)
 # Access the Redis URL provided by Heroku's environment variable
 redis_url = os.environ.get("REDISCLOUD_URL")
 redis_conn = Redis.from_url(redis_url)
+# for local use
+# redis_conn = Redis()
 task_queue = Queue("task_queue", connection=redis_conn)
 
 secret_key = os.environ.get('SECRET_KEY')
@@ -101,9 +103,9 @@ def get_spreadsheet_data():
 
         return jsonify({'message': 'Data retrieved successfully', 'data': data}), 200
     except Exception as e:
-        # If there is an error (like a failure to fetch the data), log the error and return a 500 status code
+        # If there is an error (like a failure to fetch the data), log the error and return a 400 status code
         logging.exception("Failed to fetch spreadsheet data")
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)}), 400
 
 
 @app.route('/get-doc-urls', methods=['GET'])
@@ -112,7 +114,7 @@ def get_doc_urls():
         return jsonify({'document_urls': doc_urls}), 200
     except Exception as e:
         logging.exception("Error in get-doc-urls endpoint")
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)}), 400
 
 
 def generate_content_queue(data):
@@ -124,9 +126,9 @@ def generate_content_queue(data):
     max_tokens = data.get('max_tokens', 150)  # Default to 150 if not provided
     folder_id = data.get('folder_id')
     if not selected_rows:
-        return json.dumps({'error': "Data not provided"}), 500
+        return json.dumps({'error': "Data not provided"}), 400
     if not folder_id:
-        return json.dumps({'error': "Folder Path not provided"}), 500
+        return json.dumps({'error': "Folder Path not provided"}), 400
 
     logging.debug("Received data: %s", data)
 
@@ -134,21 +136,25 @@ def generate_content_queue(data):
     for index, row in enumerate(selected_rows):
         row_data = []
         if 'Instructions' not in row:
-            return json.dumps({'error': "Instructions not provided of row " + str(row.get('row_no'))}), 500
+            return json.dumps({'error': "Instructions not provided of row " + str(row.get('row_no'))}), 400
         if 'Blog Title' not in row:
-            return json.dumps({'error': "Blog Title not provided of row " + str(row.get('row_no'))}), 500
+            return json.dumps({'error': "Blog Title not provided of row " + str(row.get('row_no'))}), 400
         instructions = formulate_instructions(row, run_number=1)
         for no, instruction in enumerate(instructions):
             content = generate_content(OPENAI_API_KEY, instruction, max_tokens)
             if hasattr(content, 'json_body'):
                 return json.dumps({'error': str(content.json_body.get('error').get('message')) + " on row " + str(
-                    row.get('row_no'))}), 500
+                    row.get('row_no'))}), 400
             else:
-                percentage_done = calculate_percentage(no + 1, len(instructions))
-                job.meta['status'] = 'in_progress'
-                job.meta["percentage_done"] = percentage_done
-                job.save()
-                row_data.append(content.get('choices')[0].get('message').get('content'))
+                try:
+                    row_data.append(content.get('choices')[0].get('message').get('content'))
+                    percentage_done = calculate_percentage(no + 1, len(instructions))
+                    job.meta['status'] = 'in_progress'
+                    job.meta["percentage_done"] = percentage_done
+                    job.save()
+                except Exception as e:
+                    return json.dumps(
+                        {'error': "Data provided by gpt is not correct please send this row again"}), 400
         rows = {
             "title": row['Blog Title'],
             "content": ''.join(row_data),
@@ -167,7 +173,7 @@ def generate_content_endpoint():
     data = request.get_json()
     from App import generate_content_queue
     job = task_queue.enqueue_call(
-        func=generate_content_queue, args=(data,), result_ttl=5000
+        func=generate_content_queue, args=(data,), result_ttl=4000
     )
     return jsonify({'task_id': job.get_id()}), 200
 
@@ -177,14 +183,20 @@ def get_results(job_key):
     job = Job.fetch(job_key, connection=redis_conn)
     job.refresh()
     if job.is_finished:
-        return str(job.result), 200
+        if isinstance(job.result, str):
+            return jsonify(job.result), 200
+        else:
+            error_message, status_code = job.result
+            # Convert the response dictionary to a JSON string
+            return jsonify(error_message), status_code
     else:
         if job.meta:
             if job.meta.get("status") == "in_progress":
-                return json.dumps({"status": "In Progress",
-                                   "percentage_done": str(job.meta.get("percentage_done"))})
+                return jsonify({"status": "In Progress",
+                                "percentage_done": str(job.meta.get("percentage_done"))}), 200
         else:
-            return json.dumps({"status": "In Progress"})
+            return jsonify({"status": "In Progress", "percentage_done": str(0)}
+                           ), 200
 
 
 def create_doc(data):
@@ -207,10 +219,11 @@ def create_doc(data):
         # log the successful update
         logging.info("Document updated successfully")
     except Exception as e:
-        return json.dumps({'error': "Data not provided"}), 500
+        return json.dumps({'error': "Data not provided"}), 400
 
     # Return the doc ID and URL
     return json.dumps({'docId': "Document created successfully " + ''.join(doc_name),
+                       'status': "Completed",
                        'docurl': "https://docs.google.com/document/d/" + str(doc_id) + "/edit"})
 
 
