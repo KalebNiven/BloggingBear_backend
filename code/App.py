16208cc9c15dd3e0ad3c58888d2e47b5a3b72bf2
unknown <kaleb.niven7@gmail.com>
2023-10-30 01:35:27 +0500

    added api to get results based on task id

--- code/App.py
+++ code/App.py
@@ -5,17 +5,30 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 import pandas as pd
-from config import cipher_suite, OPENAI_API_KEY, CORS_ORGINS
+from config import OPENAI_API_KEY
 from cryptography.fernet import Fernet
+from rq import Worker, Queue, get_current_job
+from flask_sqlalchemy import SQLAlchemy
+from redis import Redis
+from rq.job import Job
+import json
 
 
 logging.basicConfig(level=logging.DEBUG)
 
-app = Flask(__name__)
-CORS(app, origins='*')
+# testing commit
 
-# Attempt to load the secret key from an environment variable
-# If it doesn't exist, create a new one and save it in the environment variable
+app = Flask(__name__)
+# CORS(app)
+# CORS(app, resources={r"/*": {"origins": "https://bloggingbear-frontend-39f0be1ffd81.herokuapp.com/"}})
+CORS(app)
+# Access the Redis URL provided by Heroku's environment variable
+redis_url = os.environ.get("REDISCLOUD_URL")
+redis_conn = Redis.from_url(redis_url)
+# for local use
+# redis_conn = Redis()
+task_queue = Queue("task_queue", connection=redis_conn)
+
 secret_key = os.environ.get('SECRET_KEY')
 if not secret_key:
     secret_key = Fernet.generate_key().decode()
@@ -31,22 +44,29 @@ cipher_suite = Fernet(secret_key.encode())
 doc_urls = []
 
 
-
 # Create another route to get the decrypted token
 @app.route('/get_token', methods=['GET'])
 def get_token():
-    try:
-        # Get the encrypted token from your .env file
-        with open(".env", "r") as env_file:
-            for line in env_file:
-                if line.startswith('USER_TOKEN='):
-                    encrypted_token = line[len('USER_TOKEN='):-1]
+    return "ug"
+    # try:
+    #     # Get the encrypted token from your .env file
+    #     with open(".env", "r") as env_file:
+    #         for line in env_file:
+    #             if line.startswith('USER_TOKEN='):
+    #                 encrypted_token = line[len('USER_TOKEN='):-1]
+    #
+    #     # Decrypt the token
+    #     decrypted_token = cipher_suite.decrypt(encrypted_token.encode())
+    #     return jsonify({'token': decrypted_token.decode()}), 200
+    # except Exception as e:
+    #     return jsonify({'error': str(e)}), 400
 
-        # Decrypt the token
-        decrypted_token = cipher_suite.decrypt(encrypted_token.encode())
-        return jsonify({'token': decrypted_token.decode()}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+
+def calculate_percentage(completed, total):
+    if total == 0:
+        return 0  # To avoid division by zero
+    percentage = (completed / total) * 100
+    return percentage
 
 
 @app.route('/upload-file', methods=['POST'])
@@ -86,9 +106,9 @@ def get_spreadsheet_data():
 
         return jsonify({'message': 'Data retrieved successfully', 'data': data}), 200
     except Exception as e:
-        # If there is an error (like a failure to fetch the data), log the error and return a 500 status code
+        # If there is an error (like a failure to fetch the data), log the error and return a 400 status code
         logging.exception("Failed to fetch spreadsheet data")
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)}), 400
 
 
 @app.route('/get-doc-urls', methods=['GET'])
@@ -97,64 +117,110 @@ def get_doc_urls():
         return jsonify({'document_urls': doc_urls}), 200
     except Exception as e:
         logging.exception("Error in get-doc-urls endpoint")
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)}), 400
 
 
-@app.route('/generate-content', methods=['POST'])
-def generate_content_endpoint():
+def generate_content_queue(data):
+    job = get_current_job()
     logging.info("Received request at /generate-content endpoint")
     # try:
     # Get the payload from the request
-    data = request.get_json()
     selected_rows = data.get('data')
     max_tokens = data.get('max_tokens', 150)  # Default to 150 if not provided
     folder_id = data.get('folder_id')
     if not selected_rows:
-        return jsonify({'error': "Data not provided"}), 500
+        return json.dumps({'error': "Data not provided"}), 400
     if not folder_id:
-        return jsonify({'error': "Folder Path not provided"}), 500
+        return json.dumps({'error': "Folder Path not provided"}), 400
 
     logging.debug("Received data: %s", data)
 
-    # The following is a loop that goes through each selected row,
-    # formulates instructions, and then generates content using your existing function.
-    # Note that the `formulate_instructions` function is called with a made-up `run_number` argument.
-    # You might need to adjust this part to correctly derive the run_number from your data or API request.
     response_data = []
     for index, row in enumerate(selected_rows):
         row_data = []
         if 'Instructions' not in row:
-            return jsonify({'error': "Instructions not provided of row " + str(row.get('row_no'))}), 500
+            return json.dumps({'error': "Instructions not provided of row " + str(row.get('row_no'))}), 400
         if 'Blog Title' not in row:
-            return jsonify({'error': "Blog Title not provided of row " + str(row.get('row_no'))}), 500
+            return json.dumps({'error': "Blog Title not provided of row " + str(row.get('row_no'))}), 400
+        if len(row.get('Instructions')) < 1:
+            return json.dumps({'error': "Instructions not provided of row " + str(row.get('row_no'))}), 400
+        if len(row.get('Blog Title')) < 1:
+            return json.dumps({'error': "Blog Title not provided of row " + str(row.get('row_no'))}), 400
         instructions = formulate_instructions(row, run_number=1)
-        for instruction in instructions:
+        for no, instruction in enumerate(instructions):
             content = generate_content(OPENAI_API_KEY, instruction, max_tokens)
             if hasattr(content, 'json_body'):
-                return jsonify({'error': str(content.json_body.get('error').get('message')) + " on row " + str(
-                    row.get('row_no'))}), 500
+                return json.dumps({'error': str(content.json_body.get('error').get('message')) + " on row " + str(
+                    row.get('row_no'))}), 400
             else:
-                row_data.append(content.get('choices')[0].get('message').get('content'))
+                try:
+                    print("---content---")
+                    print(content)
+                    print("---content choices---")
+                    print(content["choices"])
+                    print("---content choices 0---")
+                    print(content["choices"][0])
+                    print("---content choices 0 message---")
+                    print(content["choices"][0]["message"])
+                    print("---content choices 0 message content---")
+                    print(content["choices"][0]["message"]["content"])
+                    row_data.append(content["choices"][0]["message"]["content"])
+                    percentage_done = calculate_percentage(no + 1, len(instructions))
+                    job.meta['status'] = 'in_progress'
+                    job.meta["percentage_done"] = percentage_done
+                    job.save()
+                except Exception as e:
+                    # "Data provided by gpt is not correct please send this row " + str(
+                    #     row.get('row_no')) + " again"
+                    return json.dumps(
+                        {'error': str(e)}), 400
         rows = {
             "title": row['Blog Title'],
             "content": ''.join(row_data),
             "folder_id": folder_id,
         }
         response_data.append(rows)
+        print("response data")
+        print(response_data)
 
     # Return the generated content
     return create_doc(response_data)
-    # return jsonify({'data': response_data}), 200
 
 
-# except Exception as e:
-#     # Handle any errors that occur
-#     logging.error("An error occurred: %s", str(e))
-#     return jsonify({'error': str(e)}), 500
+@app.route('/generate-content', methods=['POST'])
+def generate_content_endpoint():
+    data = request.get_json()
+    from App import generate_content_queue
+    job = task_queue.enqueue_call(
+        func=generate_content_queue, args=(data,), result_ttl=4000, timeout=1000
+    )
+    return jsonify({'task_id': job.get_id()}), 200
+
+
+@app.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+    job = Job.fetch(job_key, connection=redis_conn)
+    job.refresh()
+    if job.is_finished:
+        if isinstance(job.result, str):
+            return jsonify(json.loads(job.result)), 200
+        else:
+            error_message, status_code = job.result
+            # Convert the response dictionary to a JSON string
+            return jsonify(json.loads(error_message)), status_code
+    else:
+        if job.meta:
+            if job.meta.get("status") == "in_progress":
+                return jsonify({"status": "In Progress",
+                                "percentage_done": str(job.meta.get("percentage_done"))}), 200
+        else:
+            return jsonify({"status": "In Progress", "percentage_done": str(0)}
+                           ), 200
 
 
 def create_doc(data):
     try:
+        print("in doc function")
         doc_name = []
         for row in data:
             title = row.get('title')
@@ -162,7 +228,7 @@ def create_doc(data):
             content = row.get('content')
             # Create a new Google Doc using the function you defined earlier
             doc_id, name = create_google_doc(title, folder_id)
-            doc_name.append("," + name)
+            doc_name.append(name)
             # Log the successful creation
             logging.info("Document created successfully")
 
@@ -171,16 +237,15 @@ def create_doc(data):
 
         # log the successful update
         logging.info("Document updated successfully")
-    except KeyError as e:
-        return jsonify({'error': "Data not provided"}), 500
     except Exception as e:
-        return jsonify(error=str(e), message="An error occurred"), 500
+        return json.dumps({'error': "Data not provided"}), 400
 
     # Return the doc ID and URL
-    return jsonify({'docId': "Document created successfully " + ''.join(doc_name)})
-
+    return json.dumps({'docId': "Document created successfully " + ''.join(doc_name),
+                       'status': "Completed",
+                       'docurl': "https://docs.google.com/document/d/" + str(doc_id) + "/edit"})
 
 
 if __name__ == '__main__':
     # setup_google_docs_api()
-    app.run(debug=False)
+    app.run(debug=False, host="0.0.0.0")
